6b16836e1dc916302607176511e470e2774df47a
Zander Bailey <zbailey656@gmail.com>
2019-07-09 16:09:56 -0700

    update icon, title

--- themeter/themeter.py
+++ themeter/themeter.py
@@ -21,7 +21,7 @@ class Themeter():
             self.stopwords = pickle.load(f4) 
         topics1 = ['Action','Crime','Performance','On the Run','Relationship_Drama','Espionage','Suspense','Romance']
         topics2 = ['Fighting','Romance','Drama','On the Run','Psychological',
-               'Heist','Thriller involving Planes','Crime','Courtroom','Tough Decisions',
+               'Heist','Thriller','Crime','Courtroom','Tough Decisions',
                'Mystery','Action']
          
         keywords1 = [['ship', 'plane', 'pilot', 'flight', 
